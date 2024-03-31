931936deb3729924746bedfdb250a35d3543ffb3
Ciaran Brohan <ciaran.brohan@gmail.com>
2024-03-31 20:36:08 +0100

    added test for the update

--- test.js
+++ test.js
@@ -1,10 +1,8 @@
 const puppeteer = require("puppeteer");
-const userAgent = require("user-agents");
 
-const getMonthEvents = () => {
-  console.log("getMonthEvents: get month events");
-  return puppeteer
-    .launch({ headless: true, defaultViewport: null, executablePath: '/usr/bin/google-chrome', args: ["--no-sandbox"]  }) // defaultViewport: null
+puppeteer
+    .launch({headless: false})
+    // .launch({ headless: true, defaultViewport: null, executablePath: '/usr/bin/google-chrome', args: ["--no-sandbox"]  }) // defaultViewport: null
     .then(async (browser) => {
       const page = await browser.newPage();
       await page.goto(
@@ -14,9 +12,11 @@ const getMonthEvents = () => {
       //Wait for the page to be loaded
       await page.waitForSelector(".desktop-calendar-month-view");
 
+      
+
+
       let events = await page.evaluate(async () => {
-        var sleepNow = (delay) =>
-          new Promise((resolve) => setTimeout(resolve, delay));
+        var sleepNow = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
 
         let elements = document.getElementsByClassName("calendar-event");
         let eventObjects = [];
@@ -24,8 +24,14 @@ const getMonthEvents = () => {
         for (let i = 0; i < elements.length; i++) {
           elements[i].click();
           await sleepNow(1);
+            //console.log(window.location.href)
+            //console.log(document.querySelector('.modal__body'));
+          console.log(document.querySelector('header .title div').firstElementChild.innerText)
 
-          console.log(document.querySelector('.modal__body'));
+          var dateOnly = document.querySelector('header .title div').firstElementChild.innerText.replace(elements[i].querySelector("p.time-range").innerText, '');
+          var dateObject = new Date(dateOnly);
+          
+          
           eventObjects.push({
             title: elements[i].querySelector("h6.title")
               ? elements[i].querySelector("h6.title").innerText
@@ -34,6 +40,7 @@ const getMonthEvents = () => {
             time: elements[i].querySelector("p.time-range")
               ? elements[i].querySelector("p.time-range").innerText
               : "",
+            date: document.querySelector('header .title div') && elements[i].querySelector("p.time-range").innerText ? dateObject : null, 
             instructorImage: document.querySelector(
               '.modal__body [data-ds-component="avatar"] img'
             )
@@ -49,20 +56,19 @@ const getMonthEvents = () => {
                 ).nextElementSibling.innerText
               : "T45 Jiujitsu",
           });
+            
+          await sleepNow(1);
+          document.querySelector('.modal-modalClose').click();
         }
         await sleepNow(1 * elements.length + 1);
+        
         return eventObjects;
+        
       });
-
-      await browser.close();
+      console.log(events)
+      //await browser.close();
       return events;
     })
     .catch(function (err) {
       console.error(err);
     });
-};
-
-const month = "test";
-
-exports.month = month;
-exports.getMonthEvents = getMonthEvents;
